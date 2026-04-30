b3b8be4d263f95184d6d4e0ae42bcfc7bf861dad
portfolio-exhibition <ym47portfolio26428@gmail.com>
2026-05-01 01:04:31 +0900

    feature login #001

--- src/main/java/com/example/booking_system/controller/HelloController.java
+++ src/main/java/com/example/booking_system/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String Hello() {
         return "Hello";
     }
---
+++ src/main/java/com/example/booking_system/security/UserDetailsServiceImpl.java
@@ -0,0 +1,5 @@
+package com.example.booking_system.security;
+
+public class UserDetailsServiceImpl {
+
+}
