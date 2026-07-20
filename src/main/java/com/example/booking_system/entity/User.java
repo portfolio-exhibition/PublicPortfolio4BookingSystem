a3391f70ee531c9de9821736be0660f2fe29514c
portfolio-exhibition <ym47portfolio26428@gmail.com>
2026-07-21 03:15:04 +0900

    特定の会員をデータベースから削除する機能の実装

--- src/main/java/com/example/booking_system/entity/User.java
+++ src/main/java/com/example/booking_system/entity/User.java
@@ -1,7 +1,9 @@
 package com.example.booking_system.entity;
 
 import java.sql.Timestamp;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,6 +11,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -59,4 +62,8 @@ public class User {
 
     @Column(name = "updated_at", insertable = false, updatable = false)
     private Timestamp updatedAt;
+    
+    // user レコードを削除する為に追加
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Review> reviews;
 }
